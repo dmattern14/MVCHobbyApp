841fa9074e93d2be8738f3a81d905d7efcbc6a55
dmattern14 <130599602+dmattern14@users.noreply.github.com>
2025-02-04 11:42:41 -0700

    Initial commit

---
+++ MVCHobbyApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MVCHobbyApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ MVCHobbyApp/Views/Home/Services.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MVCHobbyApp.Views.Home;
+
+public class Services : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
